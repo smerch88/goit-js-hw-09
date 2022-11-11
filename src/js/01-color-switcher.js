bbcf48b8e3ebae8812915e5f80b03d83c583e659
smerch88 <maxarmyk@gmail.com>
2022-11-11 23:32:30 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+let timerId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onClickEventStart = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(
+    () => (document.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  startButton.disabled = true;
+  stopButton.disabled = false;
+};
+
+const onClickEventStop = () => {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+};
+
+startButton.addEventListener("click", onClickEventStart);
+stopButton.addEventListener("click", onClickEventStop);
