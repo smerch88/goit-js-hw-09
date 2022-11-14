197f78d57d3f67169d0736f0f4657e49481296e9
smerch88 <34419542+smerch88@users.noreply.github.com>
2022-11-14 14:38:19 +0000

    Deploying to gh-pages from @ smerch88/goit-js-hw-09@09e4089815a326d7eeab18cf1dc712cef4b8c5d9 🚀

---
+++ 03-promises.8437d75e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("form");let l,t,n;e.addEventListener("submit",(a=>{a.preventDefault(),console.log(e.elements.delay.value,e.elements.step.value,e.elements.amount.value),l=e.elements.delay.value,t=e.elements.step.value,n=e.elements.amount.value}));
+//# sourceMappingURL=03-promises.8437d75e.js.map
---
+++ 03-promises.e0b6b4bf.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("form");e.addEventListener("submit",(function(t){t.preventDefault(),console.log(e.elements.delay.value,e.elements.step.value,e.elements.amount.value),e.elements.delay.value,e.elements.step.value,e.elements.amount.value}))}();
+//# sourceMappingURL=03-promises.e0b6b4bf.js.map
