f2f632ab0c967a0ceec20ebb9e01ec65ddbe528c
smerch88 <maxarmyk@gmail.com>
2022-11-14 17:53:40 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,20 @@
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
 const formRef = document.querySelector("form");
-let delayRef;
-let stepRef;
-let amountRef;
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
 formRef.addEventListener("submit", (event) => {
@@ -19,7 +24,18 @@ formRef.addEventListener("submit", (event) => {
     formRef.elements.step.value,
     formRef.elements.amount.value
   );
-  delayRef = formRef.elements.delay.value;
-  stepRef = formRef.elements.step.value;
-  amountRef = formRef.elements.amount.value;
+  let delayRef = Number(formRef.elements.delay.value);
+  let stepRef = Number(formRef.elements.step.value);
+  let amountRef = Number(formRef.elements.amount.value);
+
+  for (let i = 1; i <= amountRef; i++) {
+    createPromise(i, delayRef)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayRef += stepRef;
+  }
 });
